c4af9a53b9fbea573d3b4d3e12ed98aefded6f61
itsahmadrezza <111778540+theahmadreza@users.noreply.github.com>
2022-10-21 12:18:13 +0330

    add self function and add user for chat message

--- core/chat/models.py
+++ core/chat/models.py
@@ -10,9 +10,17 @@ class Users(models.Model):
     UserPic = models.ImageField(upload_to="static/Users/pics")
     # email =
 
+    def __str__(self):
+        return f"user: '{self.user}', username:'{self.userName}'"
+
 
 class Chat(models.Model):
     # for now(temporary) users can only send a image
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE) 
     image = models.ImageField(upload_to="static/Users/send_images")
-    messages = models.CharField(max_length="64")   
+    message = models.CharField(max_length="64")   
     sendTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} send {self.message}"

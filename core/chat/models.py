888f7038d01f2881b689f1c45cc1a31ab41d46dc
itsahmadrezza <111778540+theahmadreza@users.noreply.github.com>
2022-10-21 12:07:16 +0330

    add Users model and Chat model

--- core/chat/models.py
+++ core/chat/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    userName = models.CharField(max_length="10")
+    UserPic = models.ImageField(upload_to="static/Users/pics")
+    # email =
+
+
+class Chat(models.Model):
+    # for now(temporary) users can only send a image
+    image = models.ImageField(upload_to="static/Users/send_images")
+    messages = models.CharField(max_length="64")   
